689debee3df047c7da76ca4f38d49a3598a4353a
Dev Salem <dev.salemsaleh@gmail.com>
2023-06-25 15:25:02 +0300

    added back button to error widget

--- lib/src/core/widgets/error_widget.dart
+++ lib/src/core/widgets/error_widget.dart
@@ -1,13 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quizwiz/src/core/core.dart';
 
 class CustomErrorWidget extends StatelessWidget {
   const CustomErrorWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Something went wrong"),
+    return Scaffold(
+      appBar: AppBar(
+          leading: CupertinoButton(
+              child: const Icon(Icons.arrow_back_ios_new),
+              onPressed: () => Navigator.of(context).pushNamed('/'))),
+      body: const Center(
+        child: Text(AppStrings.errorMessage),
       ),
     );
   }
