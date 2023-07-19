ebe64726d0b3ef7f7d8b4bd5cd191b9a2ae56be4
Dev Salem <dev.salemsaleh@gmail.com>
2023-07-19 22:48:43 +0300

    remove unncerssy flashcard placeholder

--- lib/src/features/cards/data/data_source/remote_data_source/remote_data_source.dart
+++ lib/src/features/cards/data/data_source/remote_data_source/remote_data_source.dart
@@ -1,7 +1,6 @@
 import 'package:quizwiz/src/core/core.dart';
 import 'package:quizwiz/src/core/errors/exceptions.dart';
 import 'package:quizwiz/src/features/cards/data/data.dart';
-import 'package:uuid/uuid.dart';
 
 abstract class BaseRemoteDataSource {
   factory BaseRemoteDataSource() => DioRemoteDataSource();
@@ -14,20 +13,6 @@ class DioRemoteDataSource implements BaseRemoteDataSource {
     try {
       final result = await DioClient.fetchChatCompletion(material);
       return List<Flashcard>.from(result.map((x) => Flashcard.fromMap(x)));
-      /* return [
-        Flashcard(
-            uuid: Uuid().v4(),
-            question: "what do you want to",
-            answer: "yes, no, no, no, no, no, no, no, no"),
-        Flashcard(
-            uuid: Uuid().v4(),
-            question: "questions read from the server",
-            answer: "answer read from the server  and read from the  server"),
-        Flashcard(
-            question: "what the hell do you want to",
-            answer: "I want to read from the server and read from the server",
-            uuid: Uuid().v4()),
-      ]; */
     } on Exception catch (e) {
       if (e is NetworkingException || e is UnexpectedNetworkException) {
         rethrow;
